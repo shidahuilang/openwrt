6912246a6ad9b94e2043126ea3d24449d8a3748e
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2022-03-26 18:38:48 +0800

    Update diy-part.sh

--- build/Lede_nanopi_r2s/diy-part.sh
+++ build/Lede_nanopi_r2s/diy-part.sh
@@ -17,8 +17,8 @@ uci set network.lan.broadcast='192.168.2.255'                               # IP
 uci set network.lan.dns='223.5.5.5 114.114.114.114'                         # DNS(多个DNS要用空格分开)
 uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
-#uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能（去掉uci前面的#生效）
-#uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释（去掉uci前面的#生效）
+uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能（去掉uci前面的#生效）
+uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释（去掉uci前面的#生效）
 uci set system.@system[0].hostname='OpenWrt'                            # 修改主机名称为OpenWrt-123
 #sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd              # 设置ttyd免帐号登录，如若开启，进入OPENWRT后可能要重启一次才生效（去掉uci前面的#生效）
 EOF

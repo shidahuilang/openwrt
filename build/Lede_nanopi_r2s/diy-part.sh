4bcbb9e184074264e78c0190db9aa33e578a039b
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-04-01 13:40:00 +0800

    Update diy-part.sh

--- build/Lede_nanopi_r2s/diy-part.sh
+++ build/Lede_nanopi_r2s/diy-part.sh
@@ -16,10 +16,6 @@ uci set network.lan.gateway='192.168.2.1'                                   # IP
 uci set network.lan.broadcast='192.168.2.255'                               # IPv4 广播
 uci set network.lan.dns='223.5.5.5 114.114.114.114'                         # DNS(多个DNS要用空格分开)
 uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
-uci set network.lan.proto='dhcpv6'                                          #开启ipv6
-uci set network.lan.ifname='@lan'                                           #开启ipv6
-uci set network.lan.reqaddress='try'                                        #开启ipv6
-uci set network.lan.reqprefix='auto'                                        #开启ipv6
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
 uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能（去掉uci前面的#生效）
 uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释（去掉uci前面的#生效）

a410920c350a30b1f51106dff210eb6d95c78574
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-04-18 22:03:49 +0800

    Update diy-part.sh

--- build/Tianling_source/diy-part.sh
+++ build/Tianling_source/diy-part.sh
@@ -20,7 +20,6 @@ uci set dhcp.@dnsmasq[0].filter_aaaa='1'                      # 禁止解析 IPv
 #uci set dhcp.lan.ignore='1'                                  # 关闭DHCP功能（去掉uci前面的#生效）
 uci set system.@system[0].hostname='OpenWrt'              # 修改主机名称为OpenWrt-123
 #uci set ttyd.@ttyd[0].command='/bin/login -f root'           # 设置ttyd免帐号登录（去掉uci前面的#生效）
-
 # 如果有用IPV6的话,可以使用以下命令创建IPV6客户端(LAN口)（去掉全部代码uci前面#号生效）
 #uci set network.ipv6=interface
 #uci set network.ipv6.proto='dhcpv6'

06698fe9d3758478a3f128ee03bbcab3dc837e15
czw1259 <75195638+czw1259@users.noreply.github.com>
2022-05-13 10:07:43 +0800

    Add files via upload

---
+++ build/Lede_nanopi_r4s/files/etc/shell-motd.d/kr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# [CTCGFW]Project-OpenWrt
+# Use it under GPLv3, please.
+# --------------------------------------------------------
+# Script for creating ACL file for each LuCI APP
+mount -o size=2000M -o remount /tmp

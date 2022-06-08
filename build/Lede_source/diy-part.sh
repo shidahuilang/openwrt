04ac6b10b67c8a6ad4dabaf78ce15a30e8d0d8ac
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-06-08 13:21:14 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -32,6 +32,8 @@ uci set firewall.@zone[0].network='lan ipv6'
 EOF
 
 
+sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-ssr-plus/Makefile
+
 # 设置 argon 为编译必选主题(可自行修改您要的,主题名称必须对,源码内必须有该主题)
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 

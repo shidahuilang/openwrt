a6caa7a6cc4866ddecc479292dcc1ee96d35f5b4
大灰狼 <jmkongdeshuai@126.com>
2022-06-22 10:06:19 +0800

    更新DIY

--- build/Lede_nanopi_r4s/files/etc/shell-motd.d/kr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# [CTCGFW]Project-OpenWrt
-# Use it under GPLv3, please.
-# --------------------------------------------------------
-# Script for creating ACL file for each LuCI APP
-mount -o size=2000M -o remount /tmp

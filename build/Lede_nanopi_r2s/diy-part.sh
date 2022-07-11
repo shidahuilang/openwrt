4e303043dca0c9a4862ddc9ccb917ef8c3ca228c
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-07-11 20:15:40 +0800

    Update diy-part.sh

--- build/Lede_nanopi_r2s/diy-part.sh
+++ build/Lede_nanopi_r2s/diy-part.sh
@@ -55,8 +55,8 @@ sed -i '/to-ports 53/d' $ZZZ_PATH
 #sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "$BASE_PATH/etc/rc.local"
 
 
-# x86机型,默认内核5.10，修改内核为5.15（源码时时变,自行根据target/linux/x86/Makefile文件修改）
-sed -i 's/PATCHVER=5.10/PATCHVER=5.18/g' target/linux/rockchip/Makefile
+# x86机型,默认内核5.15，修改内核为5.18（源码时时变,自行根据target/linux/x86/Makefile文件修改）
+sed -i 's/PATCHVER=5.15/PATCHVER=5.18/g' target/linux/rockchip/Makefile
 
 
 # K3专用，编译K3的时候只会出K3固件（其他机型也适宜,把phicomm_k3替换一下，名字要绝对正确才行）

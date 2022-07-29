df6f3c863b0d746945ed7cd0c4a5ed42b715770f
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-07-29 17:19:24 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -36,8 +36,7 @@ EOF
 
 # svn co https://github.com/shidahuilang/OpenClash package/luci-app-xiaolaoshu
 
-# 去除自带qbittorrent
-sed -i '/qbittorrent-simple_dynamic/d' Plug-in > /dev/null 2>&1
+
 
 # 设置 argon 为编译必选主题(可自行修改您要的,主题名称必须对,源码内必须有该主题)
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile

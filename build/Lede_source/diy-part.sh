a135c236af03c61a1514f88de49e6fc0154b2367
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-08-04 02:24:29 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -88,7 +88,6 @@ sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间（根据编译机型变化,自行调整需要删除的固件名称）
 cat >"$CLEAR_PATH" <<-EOF
-packages
 config.buildinfo
 feeds.buildinfo
 openwrt-x86-64-generic-kernel.bin

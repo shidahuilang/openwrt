24535a91c65754fea87047eed4584a2ed403ff65
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-08-04 23:07:44 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -61,10 +61,10 @@ echo "OpenClash_branch" > dev
 
 # 设置打包固件的机型，内核组合（可用内核是时时变化的,过老的内核就删除的，所以要选择什么内核请看说明）
 # 当前可用机型 s922x s922x-n2 s922x-reva a311d s905x3 s905x2 s905x2-km3 s905l3a s912 s912-m8s s905d s905d-ki s905x s905w s905
-
-cat >"$AMLOGIC_SH_PATH" <<-EOF
-amlogic_model=s905x3_s905x2_s905x_s905w_s905d_s922x_s912
-amlogic_kernel=5.15.25_5.10.100 -a true
+#amlogic_model=s905x3_s905x2_s905x_s905w_s905d_s922x_s912
+cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
+amlogic_model=s905d
+amlogic_kernel=5.10.100_5.4.180 -a true
 rootfs_size=960
 EOF
 

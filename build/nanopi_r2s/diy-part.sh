3730ec998356b5ed964b07aee9dd68cc0dbab01e
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-10-23 12:51:53 +0800

    Update diy-part.sh

--- build/nanopi_r2s/diy-part.sh
+++ build/nanopi_r2s/diy-part.sh
@@ -66,6 +66,8 @@ sed -i '/to-ports 53/d' $ZZZ_PATH
 # x86机型,默认内核5.15，修改内核为5.18（源码时时变,自行根据target/linux/x86/Makefile文件修改）
 sed -i 's/PATCHVER=5.19/PATCHVER=5.19/g' target/linux/rockchip/Makefile
 
+# 更改使用OpenClash的分支代码，把下面的master改成dev就使用dev分支，改master就是用master分支，改错的话就默认使用master分支
+export OpenClash_branch='master'
 
 # K3专用，编译K3的时候只会出K3固件（其他机型也适宜,把phicomm_k3替换一下，名字要绝对正确才行）
 #sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile

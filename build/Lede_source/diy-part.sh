cb1f45bec58beec75a6f8af522f151b111623eac
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-10-30 19:39:28 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -52,7 +52,7 @@ sed -i '/to-ports 53/d' "${ZZZ_PATH}"
 
 
 # 取消路由器每天跑分任务
-sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "${FIN_PATH}"
+# sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "${FIN_PATH}"
 
 
 # 修改默认内核（所有机型都适用，只要您编译的机型源码附带了其他内核，请至编译说明的第12条查看）

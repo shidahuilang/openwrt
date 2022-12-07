512e312ce59721933ed06215e5c3c979bcabbac2
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-12-07 22:02:19 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -30,7 +30,7 @@ export Remove_Firewall="0"                  # 删除DNS强制重定向53端口�
 export Cancel_running="1"                   # 取消路由器每天跑分任务
 export AdGuardHome_Core="0"                 # 编译固件增加AdGuardHome时,把核心下载好,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为开,0为关闭)
 
-export Personal_Signature="$(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想写啥就写啥，(填0为关闭)
+export Personal_Signature="大灰狼$(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想写啥就写啥，(填0为关闭)
 
 
 

83f41ea0ff48420c0262e4f62d711f871b4cf484
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-12-09 22:45:48 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -46,6 +46,8 @@ sed -i 's?"Web 管理"?"Web管理"?g' `grep -Eo '"Web 管理"' -rl ./`
 sed -i 's?"管理权"?"改密码"?g' `grep -Eo '"管理权"' -rl ./`
 sed -i 's?"带宽监控"?"监控"?g' `grep -Eo '"带宽监控"' -rl ./`
 
+# 拉取旧版OpenClash固件
+# svn co https://github.com/shidahuilang/OpenClash package/luci-app-xiaolaoshu
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF

91fca11d314e9b4f88b28e294bde696cb2f9fc94
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-12-16 18:15:30 +0800

    Add files via upload

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,50 +4,63 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+# 后台IP设置
+export Ipv4_ipaddr="192.168.2.1"            # 修改openwrt后台地址(填0为关闭)
+export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
+export Op_name="OpenWrt"                    # 修改主机名称为OpenWrt-123(填0为不作修改)
 
-export IPv4_ipaddr="192.168.2.1"           # 修改openwrt后台地址(填0为关闭)
-export Netmask_netm="255.255.255.0"        # IPv4 子网掩码（默认：255.255.255.0）(填0为关闭)
-export Op_name="OpenWrt"                   # 修改主机名称为OpenWrt-123(填0为关闭)
+# 默认主题设置
+export Mandatory_theme="argon"              # 将bootstrap替换您需要的主题为必选主题(可自行更改您要的,源码要带此主题就行,填写名称也要写对) (填写主题名称,填0为不作修改)
+export Default_theme="argon"                # 多主题时,选择某主题为默认第一主题 (填写主题名称,填0为不作修改)
 
-export Router_gateway="192.168.2.1"         # 旁路由设置 IPv4 网关(填0为关闭)
-export Lan_DNS="114.114.114.114 223.5.5.5"  # 旁路由设置 DNS(多个DNS要用空格分开)(填0为关闭)
-export IPv4_Broadcast="192.168.2.255"       # 设置 IPv4 广播(填0为关闭)
-export Close_DHCP="1"                       # 旁路由关闭DHCP功能(1为开,0为关闭)
-export Delete_Bridge="0"                    # 旁路由去掉桥接模式(1为开,0为关闭)
-export ttyd_Nopassword="0"                  # 设置ttyd免密登录(1为开,0为关闭)
+# 旁路由选项
+export Gateway_Settings="0"                 # 旁路由设置 IPv4 网关(填入您的网关IP为启用)(填0为不作修改)
+export DNS_Settings="0"                     # 旁路由设置 DNS(填入DNS，多个DNS要用空格分开)(填0为不作修改)
+export Broadcast_Ipv4="0"                   # 设置 IPv4 广播(填入您的IP为启用)(填0为不作修改)
+export Disable_DHCP="0"                     # 旁路由关闭DHCP功能(1为启用命令,填0为不作修改)
+export Disable_Bridge="0"                   # 旁路由去掉桥接模式(1为启用命令,填0为不作修改)
+export Create_Ipv6_Lan="0"                  # 爱快+OP双系统时,爱快接管IPV6,在OP创建IPV6的lan口接收IPV6信息(1为启用命令,填0为不作修改)
 
-export Package_IPv6helper="1"               # 编译IPV6固件(1为开,0为关闭)
-export Remove_IPv6="1"                      # 关闭固件里面所有IPv6选项和IPv6的DNS解析记录(1为开,0为关闭)
-export Create_IPV6_interface="1"            # 爱快+OP双系统时,爱快接管IPV6,在OP创建IPV6的lan口接收IPV6信息(1为开,0为关闭)
-export OpenClash_branch="master"            # OpenClash代码选择分支（master 或 dev）(填0为不需要此插件)
-export OpenClash_Core="1"                   # 编译固件增加OpenClash时,把核心下载好,核心为3MB左右大小(1为开,0为关闭)
-export Required_Topic="argon"               # 将bootstrap替换您需要的主题为必选主题,名称必须写对,源码内必须有该主题(填0为关闭)
-export Default_Theme="argon"                # 多主题时,选择某主题为默认第一主题 (填写主题名称,填0为关闭)
-export Delete_NotRequired="1"               # 个别机型内一堆其他机型固件,删除其他机型的,只保留当前主机型固件(1为开,0为关闭)
-export Kernel_Patchver="6.1"                # 更换内核版本,前提是您编译的机型源码内保证有其他内核存在(0为关闭,写上其他表示替换)
-export Confidentiality_free="1"             # 设置首次登录后台密码为空（进入openwrt后自行修改密码）(1为开,0为关闭)
-export Remove_Firewall="0"                  # 删除DNS强制重定向53端口防火墙规则(1为开,0为关闭)
-export Cancel_running="1"                   # 取消路由器每天跑分任务
-export AdGuardHome_Core="1"                 # 编译固件增加AdGuardHome时,把核心下载好,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为开,0为关闭)
+# IPV6
+export Enable_IPV6_function="0"              # 编译IPV6固件(1为启用命令,填0为不作修改)
+export Disable_IPv6_option="0"               # 关闭固件里面所有IPv6选项和IPv6的DNS解析记录(1为启用命令,填0为不作修改)
 
-export Personal_Signature="大灰狼 $(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想写啥就写啥，(填0为关闭)
+# OpenClash
+export OpenClash_branch="master"             # OpenClash代码选择分支（master 或 dev）(填0为不需要此插件)
+export OpenClash_Core="1"                    # 编译固件增加OpenClash时,把核心下载好,核心为3MB左右大小(1为启用命令,填0为不需要核心)
+
+# 个性签名,默认增加年月日[$(TZ=UTC-8 date "+%Y.%m.%d")]
+export Customized_Information="大灰狼 $(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想写啥就写啥，(填0为不作修改)
+
+# 更换固件内核
+export Replace_Kernel="6.1"                    # 更换内核版本,在对应源码的[target/linux/架构]查看patches-x.x,看看x.x有啥就有啥内核了(填入内核版本号,填0为不作修改)
+
+# 设置免密码登录(个别源码本身就没密码的)
+export Password_free_login="0"               # 设置首次登录后台密码为空（进入openwrt后自行修改密码）(1为启用命令,填0为不作修改)
+
+# 增加AdGuardHome插件时把核心一起下载好
+export AdGuardHome_Core="0"                  # 编译固件增加AdGuardHome时,把核心下载好,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为启用命令,填0为不需要核心)
+
+# 其他
+export Ttyd_account_free_login="0"           # 设置ttyd免密登录(1为启用命令,填0为不作修改)
+export Delete_unnecessary_items="0"          # 个别机型内一堆其他机型固件,删除其他机型的,只保留当前主机型固件(1为启用命令,填0为不作修改)
+export Disable_53_redirection="0"            # 删除DNS强制重定向53端口防火墙规则(个别源码本身不带次功能)(1为启用命令,填0为不作修改)
+export Cancel_running="0"                    # 取消路由器每天跑分任务(个别源码本身不带次功能)(1为启用命令,填0为不作修改)
 
 
 
 # 修改插件名字
-sed -i 's?"aMule设置"?"电驴下载"?g' `grep -Eo '"aMule设置"' -rl ./`
-sed -i 's?"网络存储"?"NAS"?g' `grep -Eo '"网络存储"' -rl ./`
-sed -i 's?"Turbo ACC 网络加速"?"网络加速"?g' `grep -Eo '"Turbo ACC 网络加速"' -rl ./`
-sed -i 's?"实时流量监测"?"流量"?g' `grep -Eo '"实时流量监测"' -rl ./`
-sed -i 's?"KMS 服务器"?"KMS激活"?g' `grep -Eo '"KMS 服务器"' -rl ./`
-sed -i 's?"TTYD 终端"?"TTYD"?g' `grep -Eo '"TTYD 终端"' -rl ./`
-sed -i 's?"USB 打印服务器"?"打印服务"?g' `grep -Eo '"USB 打印服务器"' -rl ./`
-sed -i 's?"Web 管理"?"Web管理"?g' `grep -Eo '"Web 管理"' -rl ./`
-sed -i 's?"管理权"?"改密码"?g' `grep -Eo '"管理权"' -rl ./`
-sed -i 's?"带宽监控"?"监控"?g' `grep -Eo '"带宽监控"' -rl ./`
+sed -i 's/"aMule设置"/"电驴下载"/g' `egrep "aMule设置" -rl ./`
+sed -i 's/"网络存储"/"NAS"/g' `egrep "网络存储" -rl ./`
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `egrep "Turbo ACC 网络加速" -rl ./`
+sed -i 's/"实时流量监测"/"流量"/g' `egrep "实时流量监测" -rl ./`
+sed -i 's/"KMS 服务器"/"KMS激活"/g' `egrep "KMS 服务器" -rl ./`
+sed -i 's/"TTYD 终端"/"命令窗"/g' `egrep "TTYD 终端" -rl ./`
+sed -i 's/"USB 打印服务器"/"打印服务"/g' `egrep "USB 打印服务器" -rl ./`
+sed -i 's/"Web 管理"/"Web管理"/g' `egrep "Web 管理" -rl ./`
+sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
+sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
 
-# 拉取旧版OpenClash固件
-# svn co https://github.com/shidahuilang/OpenClash package/luci-app-xiaolaoshu
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF

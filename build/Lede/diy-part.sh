2f38b7fb6f8154d8db9191ae2860ffe38b42b4d4
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2024-12-29 17:09:16 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -67,7 +67,7 @@ export Disable_autosamba="0"                 # 去掉源码默认自选的luci-a
 # 手动更换默认主题
 # uci set luci.main.mediaurlbase='/luci-static/neobird' && uci commit luci
 sed -i 's@src-git luci@# src-git luci@g' feeds.conf.default
-sed -i 's@#src-git luci@src-git luci@g' feeds.conf.default
+sed -i 's@## src-git luci@src-git luci@g' feeds.conf.default
 
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
